e610063cedb03e4f7fc6e78d003007e6feda8550
karlmis <karlmissiaen@gmail.com>
2022-04-21 18:01:18 +0200

    10.122 add ingredient to Recipe

--- prj-start/src/app/recipes/recipe.service.ts
+++ prj-start/src/app/recipes/recipe.service.ts
@@ -1,13 +1,25 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/Ingredient.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Recipe.jpg/714px-Recipe.jpg?20170213105318'),
-    new Recipe('A Test Recipe1', 'This is simply a test1', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Recipe.jpg/714px-Recipe.jpg?20170213105318')
+    new Recipe(
+      'A Test Recipe'
+      , 'This is simply a test'
+      , 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Recipe.jpg/714px-Recipe.jpg?20170213105318'
+      , [new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('A Test Recipe1'
+      , 'This is simply a test1'
+      , 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Recipe.jpg/714px-Recipe.jpg?20170213105318'
+      , [new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
   recipeSelected = new EventEmitter<Recipe>();
 
