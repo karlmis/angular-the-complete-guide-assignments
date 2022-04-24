05381dca1f6e6b421aa57fe9cd7c270545625176
karlmis <karlmissiaen@gmail.com>
2022-04-24 15:37:24 +0200

    12.165 add recipe-edit, pay attention to the order in routes: /new before /:id

--- prj-start/src/app/app.module.ts
+++ prj-start/src/app/app.module.ts
@@ -14,12 +14,13 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
 import {DropdownDirective} from './shared/dropdown.directive';
 import {AppRoutingModule} from './app-routing.module';
 import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent, HeaderComponent, RecipesComponent, RecipeListComponent
     , RecipeDetailComponent, RecipeItemComponent, ShoppingListComponent
-    , ShoppingEditComponent, DropdownDirective, RecipeStartComponent
+    , ShoppingEditComponent, DropdownDirective, RecipeStartComponent, RecipeEditComponent
   ],
   imports: [
     BrowserModule,
