9cfb0081d2b9a3d7328f6a951deca08dd66e0f74
karlmis <karlmissiaen@gmail.com>
2022-04-26 17:05:52 +0200

    assignment 7: solution

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  inputForm: FormGroup;
+  statusses = ['Stable', 'Critical', 'Finished'];
+
+  ngOnInit(): void {
+    this.inputForm = new FormGroup({
+      // 'projectName': new FormControl(null, [Validators.required, this.testNotAllowed]),
+      'projectName': new FormControl(null, Validators.required, this.testNotAllowedAsync),
+      'mail': new FormControl(null, [Validators.required, Validators.email]),
+      'status': new FormControl(null)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.inputForm);
+  }
+
+  testNotAllowed(formControl: FormControl): { [s: string]: boolean } {
+    if (formControl.value === 'Test') {
+      return {projectNameNotAllowed: true};
+    }
+    return null;
+  }
+
+  testNotAllowedAsync(formControl: FormControl): Promise<any> | Observable<any> {
+    return new Promise<any>((resolve, reject) => {
+        setTimeout(() => {
+          if (formControl.value === 'Test') {
+            resolve({projectNameNotAllowed: true});
+          }
+          resolve(null);
+        }, 1500);
+      }
+    );
+  }
+
+
 }
